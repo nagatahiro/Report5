32ab771e44c7ab5e3a875712bb2ddbd561694ade
nagatahira hiroyuki <e225740@ie.u-ryukyu.ac.jp>
2022-12-11 20:22:04 +0900

    step2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
+
 public class Main {
 
+
+    //ステップ１
     public static void main(String[] args){
+    try{
         String str1 = null;
         System.out.println( str1.length());
 
+    }catch (NullPointerException e){
+        System.out.println("NullPointerExceptionが発生しました") ;
+        System.out.println(  e.getMessage());
+
+
     }
+}
     
 }
+
